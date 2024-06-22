a70a5f75e2474f0db2c72980a614937f56861cd6
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-06-22 16:24:49 +0300

    problem that OCP will solve it

---
+++ OCP/Problem/src/Employee.java
@@ -0,0 +1,15 @@
+ public class Employee {
+        private String name;
+        private double monthlySalary;
+
+        public Employee(String name, double monthlySalary) {
+            this.name = name;
+            this.monthlySalary = monthlySalary;
+        }
+
+        public double calculateSalary() {
+            return monthlySalary;
+        }
+    }
+
+
---
+++ OCP/Problem/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee=new Employee("mahmud",124d);
+        System.out.println(employee.calculateSalary());
+    }
+}
